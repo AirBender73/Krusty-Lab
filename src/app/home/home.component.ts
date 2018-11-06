d691a8a986ef1610534ace6ad8f8922020d87c0c
David Montores <daymon.117@gmail.com>
2018-11-06 14:57:31 -0600

    Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,12 +6,99 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  //PARTICLES
+  myStyle: object = {};
+    myParams: object = {};
+    width: number = 100;
+    height: number = 100;
+
+  mostrarIconos: boolean = true;
+  animationIconos: boolean = false;
+  acelerarAtomo: boolean = false;
+
+  //AUDIO
+  backgrounMuted: boolean = false;
+
 
   constructor() {
     
    }
 
   ngOnInit() {
+    this.audioBackgroung();
+    this.particles();
+  }
+
+  ocultarIconos(){
+    this.animationIconos = true;
+    this.acelerarAtomo = true;
+    this.playAtomo();
+    
+    setTimeout(() => {
+      this.mostrarIconos = false;
+      this.animationIconos = false;  
+    }, 750);
+    setTimeout(() => {
+      this.acelerarAtomo = false;  
+    }, 1000);
+  } 
+  
+  audioBackgroung(){
+    let audio:any = document.getElementById("backgroundSound");
+    audio.autoplay = true;
+    audio.loop = true;
+    audio.load();
+  }
+  
+  playAtomo(){
+    console.log("funcion play");
+    var audioElect:any = document.getElementById("soundElect");
+    audioElect.muted = false;
+    audioElect.autoplay = true;
+    audioElect.load();
+
+    setTimeout(() => {
+      audioElect.muted = true;
+    }, 1500);
+  }
+
+  muted(){
+    let audio:any = document.getElementById("backgroundSound");
+    if(this.backgrounMuted){
+      audio.muted = false;
+      this.backgrounMuted = false;
+    }else{
+      audio.muted = true;
+      this.backgrounMuted = true;
+    }
+  }
+
+  particles(){
+    this.myStyle = {
+      'position': 'fixed',
+      'width': '100%',
+      'height': '100%',
+      'z-index': -1,
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+    };
+
+this.myParams = {
+      particles: {
+          number: {
+              value: 50,
+          },
+          color: {
+              value: '#ffffff'
+          },
+          shape: {
+              type: 'circle',
+          },
+      }
+    };
   }
 
 
