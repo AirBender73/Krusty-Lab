2e7db0b8bff45c8ee801f4a72fc2753d7b69493b
Unknown <daymon.117@gmail.com>
2018-11-21 12:26:11 -0600

    Services

--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -8,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class ServiciosComponent implements OnInit {
   
   sitios: boolean = false;
+  appWeb: boolean = false;
+  lanPage: boolean = false;
   app: boolean = false;
+  appMovilH: boolean = false;
+  appMovilN: boolean = false;
   db: boolean = false;
+  dbTR: boolean = false;
+  dbSQL: boolean = false;
+  dbNoSQL: boolean = false;
 
   constructor() { }
 
@@ -17,18 +24,46 @@ export class ServiciosComponent implements OnInit {
 
   show(option){
     this.sitios = false;
+    this.appWeb = false;
+    this.lanPage = false;
     this.app = false;
+    this.appMovilH = false;
+    this.appMovilN = false
     this.db = false;
+    this.dbTR = false;
+    this.dbSQL = false;
+    this.dbNoSQL = false;
     setTimeout(() => {
       switch (option){
         case 'sitios':
           this.sitios = true;
         break;
+        case 'appWeb':
+          this.appWeb = true;
+        break;
+        case 'lanPage':
+          this.lanPage = true;
+        break;
         case 'app':
           this.app = true;
         break;
+        case 'appMovilH':
+          this.appMovilH = true;
+        break;
+        case 'appMovilN':
+          this.appMovilN = true;
+        break;
         case 'db':
           this.db = true;
+        break;
+        case 'dbTR':
+          this.dbTR = true;
+        break;
+        case 'dbSQL':
+          this.dbSQL = true;
+        break;
+        case 'dbNoSQL':
+          this.dbNoSQL = true;
         break;
       }
     }, 50);
